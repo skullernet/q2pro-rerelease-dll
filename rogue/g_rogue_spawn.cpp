a7db7cb7fe43e89b399826cff0d6906b45a088cc
Andrey Nazarov <skuller@skuller.net>
2023-10-04 15:35:37 +0300

    Sync with upstream: lerp() function.

--- rogue/g_rogue_spawn.cpp
+++ rogue/g_rogue_spawn.cpp
@@ -167,7 +167,7 @@ THINK(spawngrow_think)(edict_t *self) -> void {
 
     float t = 1.f - ((level.time - self->teleport_time).seconds() / self->wait);
 
-    self->x.scale = clamp(lerp(self->accel, self->decel, t) / 16.f, 0.001f, 16.f);
+    self->x.scale = clamp(lerp(self->decel, self->accel, t) / 16.f, 0.001f, 16.f);
     self->x.alpha = t * t;
 
     self->nextthink += FRAME_TIME;
